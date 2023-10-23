acb7b73dbb78dc2979e16da66c714e5104cba624
Arnoldelite <arnold.elite@ssense.com>
2023-10-23 13:27:00 -0400

    - update readme  - clean code

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,6 +2,7 @@ import React, {  useState } from 'react';
 import NoteForm from './NoteForm';
 import Tag from './Tag';
 import { RiCloseCircleLine } from 'react-icons/ri';
+import { RiFile3Line } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 
 
@@ -32,6 +33,7 @@ const Note = ({ notes, completeNote, removeNote, updateNote}) => {
 	   <div key={note.id} data-testid="note-content" onClick={() => completeNote(note.id)}>{note.text}</div>
 	   <div className="icons">
 	   		{note.isValid ? <Tag name={"Valid Format"}/> : <Tag name={"Invalid Format"}/>}
+		   {note.isFile && <RiFile3Line data-testid="edit-icon"  className="edit-icon" />}
 		   <TiEdit data-testid="edit-icon" onClick={() => setEdit({ id: note.id, value: note.text})} className="edit-icon" />
 		   <RiCloseCircleLine data-testid="delete-icon" onClick={() => removeNote(note.id)} className="delete-icon" />
 	   </div>
--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect, useRef } from 'react';
 function NoteForm(props) {
 	const [input, setInput] = useState('');
 	const [isValid, setIsValid] = useState(false);
+	const [isFile, setIsFile] = useState(false);
+	const [error, setError] = useState(false);
 	const inputRef = useRef(null);
 
 	useEffect(() => {
@@ -11,44 +13,92 @@ function NoteForm(props) {
 
 	const handleChange = (e) => {
 		setInput(e.target.value);
+		var inputElement = document.getElementsByName('url');
+		let check = inputElement[0].checkValidity();
+		setIsValid(check);
+		setError(false);
 	};
 
+	//mock server call with async function that returns a promise
+	function isValidHttpUrl(urlToBeTested) {
+		console.log('passed in string >>', urlToBeTested);
+		return new Promise((resolve, reject) => {
 
-	  function isValidHttpUrl(string) {
 		try {
-			const url = new URL(string);
-			  var inputElement = document.getElementsByName('url');
-			  let check = inputElement[0].checkValidity();
-			// return url.protocol === "http:" || url.protocol === "https:";
-		  return Boolean(url) || check; 
+			if (typeof urlToBeTested !== 'string') {
+				return reject(new TypeError('Expected a string'));
+			}
+		
+			urlToBeTested = urlToBeTested.trim();
+			if (urlToBeTested.includes(' ')) {
+				return reject(false);
+			}
+
+			const url = new URL(urlToBeTested);
+		
+			return resolve(url.protocol === "http:" || url.protocol === "https:");
+	
 
 		} catch (err) {
-			console.log("Invalid url error!", err);
-		  return false;
+			if (err.message) {
+				console.log("Invalid url error!", {error: err});
+				setError(true);
+				setIsValid(false);
+				setIsFile(false);
+			}
+		   throw err;
 		}
-
+		});
 	  }
+
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		// e.persist();
 
-		const check = isValidHttpUrl(input);
-		setIsValid(check);
+		isValidHttpUrl(input)
+		.then((result) => {		
+			setIsValid(result);
+			const parts = input.split('/');
+			console.log('check file type >>', input);
+  			const lastPart = parts[parts.length - 1];
 
-		props.onSubmit({
-			id: Math.floor(Math.random() * 10000),
-			text: input,
-			isValid: isValid,
+			// Check if the last part has a file extension (e.g., ends with .txt, .pdf, .xlsx or .json) this could also be checked using regex
+			const checkFile = lastPart.includes('.txt') || lastPart.includes('.pdf')  || lastPart.includes('.xlsx') || lastPart.includes('.json');
+			setIsFile(checkFile);
+			props.onSubmit({
+				id: Math.floor(Math.random() * 10000),
+				text: input,
+				isValid: isValid,
+				isFile: checkFile,
+			});
+		})
+		.catch((error) => {
+			if (error) {
+				
+				setIsValid(false);
+				setIsFile(false);
+				props.onSubmit({
+					id: Math.floor(Math.random() * 10000),
+					text: input,
+					isValid: false,
+					isFile: false,
+				});
+			
+			console.log(`Couldn't check url format: ${error}`)
+			}
+			
 		});
 
 		setInput('');
+		setIsFile(false);
+		setIsValid(false);
 	};
 
 	return (
 		<form className="note-form" onSubmit={handleSubmit}>
-			<input type="url" placeholder={ props.edit ? 'edit existing url!' : 'Enter url to verify!'} value={input} data-testid="add-note" name='url' className='note-input' onChange={handleChange} ref={inputRef}/>
-			<button className="note-button"> { props.edit ? 'edit url entry!' : 'Verify url!'} </button>
+			<input type="text" placeholder={ props.edit ? 'edit existing url!' : 'Enter url to verify!'} value={input} data-testid="add-note" name='url' className='note-input' onChange={handleChange} ref={inputRef}/>
+			<button disabled={input === ''} className="note-button"> { props.edit ? 'edit url entry!' : 'Verify url!'} </button>
 		</form>
 	);
 
